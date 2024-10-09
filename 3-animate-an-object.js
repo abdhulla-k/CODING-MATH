e48cbc35c6c0f87c3f631ed7b7d519b42a53496d
abdhulla-k <abdhullak149@gmail.com>
2024-10-09 23:07:41 +0530

    created an object and moved it from top <=> bottom infinitly using sin

--- 3-animate-an-object.js
+++ 3-animate-an-object.js
@@ -13,19 +13,27 @@ window.onload = () => {
         speed = 0.01,
         angle = 0;
 
+    // Create the object and animate it.
     render();
 
     function render() {
+        // find the y axis position to move the object up and down.
+        // Use sin to animate it between 1 and -1
         let y = centerY + Math.sin(angle) * offset;
-
+        
+        // Clear the context before drowing the object. it is a Recursion function
+        // and there will be an object in the context allways
         context.clearRect(0, 0, width, height);
-
+        
+        // Drow the path
         context.beginPath();
         context.arc(centerX, y, 50, 0, Math.PI * 2, false);
         context.fill();
-
+        
+        // Incriment the angle to move the object when calling the function again
         angle += speed;
-
+        
+        // Implimenting Recursion to animate the object
         requestAnimationFrame(render);
     }
 }
