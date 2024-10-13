3054ea50546104888485db466dae78b2c825d2ab
abdhulla-k <abdhullak149@gmail.com>
2024-10-13 10:41:36 +0530

    placed some dots around a circle

--- script/4-circle.js
+++ script/4-circle.js
@@ -6,19 +6,24 @@ window.onload = () =>  {
 
     //
     let centerX = width * .5,
-          centerY = height * .5,
-          radius = 200,
-          angle = 0,
-          speed = 0.01;
-          x = undefined,
-          y = undefined;
+        centerY = height * .5,
+        radius = 200,
+        angle = 0,
+        speed = 0.01,
+        x = undefined,
+        y = undefined;
+          
+
+    // Place dots aroud the circle
+    placeDotesAroundCircle();
 
     // Draw the circle
     render();
 
+    // Function to animate an element around a circle
     function render() {
         // Clear the context
-        context.clearRect(0, 0, width, height);
+        context.clearRect(x, y, 10, 10);
 
         // Find the x and y position using sin and cos
         x = centerX + Math.cos(angle) * radius;
@@ -34,5 +39,27 @@ window.onload = () =>  {
 
         // Call the function again
         requestAnimationFrame(render);
+    }
+
+    // Funciton to place dotes around a circle
+    function placeDotesAroundCircle() {
+        let numObjects = 20;
+        let slice = Math.PI * 2 / numObjects;
+        let radius = 400;
+
+        for(let i = 0; i <= numObjects; i++) {
+            let angle = i * slice;
+
+            let x = centerX + Math.cos(angle) * radius;
+            let y = centerY + Math.sin(angle) * radius;
+
+            console.log(x, y);
+
+            context.beginPath();
+            context.arc(x, y, 10, 0, Math.PI * 2, false);
+            context.fill();
+
+            angle += slice;
+        }
     }
 }
